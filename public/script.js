5b8333b597587ef52688a8b335322e45cf9179ed
MariaBeatrizCarneiro <up202107798@up.pt>
2023-12-07 01:03:13 +0000

    Nav bar margins

--- public/script.js
+++ public/script.js
@@ -1,5 +1,11 @@
 function toggleMenu() {
     const navList = document.querySelector('.nav-list');
     navList.classList.toggle('show');
+
+    const navLinks = document.querySelectorAll('.nav-list a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navList.classList.remove('show');
+        });
+    });
   }
-  
